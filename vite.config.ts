d2c90fbf94c43f932ebce02fcac4aa5f9d0e3513
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-03-27 12:25:46 -0300

    added resolver for cookie package because pnpm was breaking the build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [
+        sveltekit(),
+        {
+            name: 'cookie-resolver',
+            resolveId(id) {
+                if (id === 'cookie') {
+                    return require.resolve('./node_modules/cookie/index.js');
+                }
+            }
+        }
+    ],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     },
