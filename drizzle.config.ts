c2aa2232456f8ca3560d1ce93d43fc534247a986
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-10 21:43:19 -0300

    formatting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["upload-t3-2_*"],
+	schema: "./src/server/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	tablesFilter: ["upload-t3-2_*"],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,13 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { index, pgTableCreator, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -19,18 +13,16 @@ import {
 export const createTable = pgTableCreator((name) => `upload-t3-2_${name}`);
 
 export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+	"post",
+	{
+		id: serial("id").primaryKey(),
+		name: varchar("name", { length: 256 }),
+		createdAt: timestamp("created_at", { withTimezone: true })
+			.default(sql`CURRENT_TIMESTAMP`)
+			.notNull(),
+		updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(() => new Date()),
+	},
+	(example) => ({
+		nameIndex: index("name_idx").on(example.name),
+	}),
 );
