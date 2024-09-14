e233a962590b2140da7428281a435ce6973e189b
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-14 18:40:21 -0300

    .

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,7 +23,7 @@ const config = {
 		"@typescript-eslint/no-unused-vars": [
 			"warn",
 			{
-				argsIgnorePattern: "^_",
+				argsIgnorePattern: "^_|^props$",
 			},
 		],
 		"@typescript-eslint/require-await": "off",
