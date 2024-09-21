554ee6724c604630a842f3a7d2807f7c09bfdd84
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-21 18:26:22 -0300

    moved hydrate client outside of layout

--- src/app/_components/settings/index.tsx
+++ src/app/_components/settings/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SettingsButton from "~/app/_components/settings/settingsButton";
 import CookieSetter from "~/app/_components/cookieSetter";
-import { api, HydrateClient } from "~/trpc/server";
+import { api } from "~/trpc/server";
 
 async function Settings() {
 	const user = await api.user.get();
@@ -10,10 +10,8 @@ async function Settings() {
 
 	return (
 		<>
-			<HydrateClient>
-				<SettingsButton />
-				<CookieSetter name="sessionId" value={user.sessionId} />
-			</HydrateClient>
+			<SettingsButton />
+			<CookieSetter name="sessionId" value={user.sessionId} />
 		</>
 	);
 }
