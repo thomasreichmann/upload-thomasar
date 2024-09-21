8024cbec4760faad9031f47c5fa86c623439ea6c
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-21 19:30:26 -0300

    test removing most code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import { Paper } from "@mui/material";
-import React, { Suspense } from "react";
-import Files from "~/app/_components/files";
-import UploadButton from "~/app/_components/upload/uploadButton";
+import React from "react";
 
 export default async function Home() {
 	return (
@@ -9,12 +6,12 @@ export default async function Home() {
 			<h1>
 				Test of a non-client component that for sure should be rendered in the front-end
 			</h1>
-			<UploadButton />
-			<Paper className="p-3">
-				<Suspense fallback="loading">
-					<Files />
-				</Suspense>
-			</Paper>
+			{/*<UploadButton />*/}
+			{/*<Paper className="p-3">*/}
+			{/*	<Suspense fallback="loading">*/}
+			{/*		<Files />*/}
+			{/*	</Suspense>*/}
+			{/*</Paper>*/}
 		</main>
 	);
 }
