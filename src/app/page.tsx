e21f4daf827ca0caf1d41a244a1b729cdcb19e08
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-21 19:59:00 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Paper } from "@mui/material";
 import React from "react";
 import UploadButton from "~/app/_components/upload/uploadButton";
+import FileList from "~/app/_components/files/fileList";
 
 export default async function Home() {
 	return (
@@ -9,7 +10,10 @@ export default async function Home() {
 				Test of a non-client component that for sure should be rendered in the front-end
 			</h1>
 			<UploadButton />
-			<Paper className="p-3">{/*<Files />*/}</Paper>
+			<Paper className="p-3">
+				{/*<Files />*/}
+				<FileList />
+			</Paper>
 		</main>
 	);
 }
