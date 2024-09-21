325c893b3ea6484936620aa0cb79858d5ffe5e54
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-21 13:35:33 -0300

    moved type def to bottom of file

--- src/app/_hooks/useUpload.ts
+++ src/app/_hooks/useUpload.ts
@@ -4,18 +4,6 @@ import { api } from "~/trpc/react";
 import { useUppyState } from "@uppy/react";
 import { useState } from "react";
 
-export interface UploadStatus {
-	isUploading: boolean;
-}
-
-type UploadMutations = {
-	createMutation: ReturnType<typeof api.upload.createMultiPartUpload.useMutation>;
-	abortMutation: ReturnType<typeof api.upload.abortMultipartUpload.useMutation>;
-	listPartMutation: ReturnType<typeof api.upload.listParts.useMutation>;
-	completeMutation: ReturnType<typeof api.upload.completeMultipartUpload.useMutation>;
-	signPartMutation: ReturnType<typeof api.upload.signPart.useMutation>;
-};
-
 export default function useUpload() {
 	// Call hooks at the top level of the hook
 	const createMutation = api.upload.createMultiPartUpload.useMutation();
@@ -37,24 +25,8 @@ export default function useUpload() {
 
 	const state = useUppyState(uppy, (state) => state);
 
-	const uploadController = {
-		cancel: async () => {
-			throw new Error("TODO");
-		},
-		pause: async () => {
-			throw new Error("TODO");
-		},
-		resume: async () => {
-			throw new Error("TODO");
-		},
-	};
-
-	const upload = async () => {};
-
 	return {
 		state,
-		uploadController,
-		upload,
 		uppy,
 	};
 }
@@ -116,3 +88,11 @@ function createUppyClient({
 		},
 	});
 }
+
+type UploadMutations = {
+	createMutation: ReturnType<typeof api.upload.createMultiPartUpload.useMutation>;
+	abortMutation: ReturnType<typeof api.upload.abortMultipartUpload.useMutation>;
+	listPartMutation: ReturnType<typeof api.upload.listParts.useMutation>;
+	completeMutation: ReturnType<typeof api.upload.completeMultipartUpload.useMutation>;
+	signPartMutation: ReturnType<typeof api.upload.signPart.useMutation>;
+};
