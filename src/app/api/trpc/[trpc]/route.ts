9671b2d7a833e500ae1c2d8983832489d9461ff4
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-09-21 17:30:10 -0300

    Revert "forcing error to be shown"
    This reverts commit 98917270d08004366571ba8eae8cdafc17710eb3.

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -22,7 +22,7 @@ const handler = (req: NextRequest) =>
 		router: appRouter,
 		createContext: () => createContext(req),
 		onError:
-			env.NODE_ENV === "development" || true
+			env.NODE_ENV === "development"
 				? ({ path, error }) => {
 						console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
 					}
